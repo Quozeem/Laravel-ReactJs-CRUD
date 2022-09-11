d8c55e04acb2c4722db94cfa1346926fdfea4d97
Ademola Quozeem <72305068+Quozeem@users.noreply.github.com>
2022-09-11 13:47:21 +0800

    Create Deleteform.jsx

---
+++ React-frontend/src/component/Deleteform.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Deleteform = () => {
+  return (
+    <>
+      <h3>Are you sure you want to delete</h3>
+      <form>
+        <input type="text" value="" />
+      </form>
+    </>
+  );
+};
+export default Deleteform;
