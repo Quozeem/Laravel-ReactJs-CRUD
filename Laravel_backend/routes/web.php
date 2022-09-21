0c9e57e058925e6c46797f5a2c72e35539723dfc
Ademola Quozeem <72305068+Quozeem@users.noreply.github.com>
2022-09-21 19:42:22 +0100

    Update web.php

--- Laravel_backend/routes/web.php
+++ Laravel_backend/routes/web.php
@@ -19,8 +19,6 @@ Route::get('/me', function () {
 
 Auth::routes();
 
-Route::post('/all_categories',[App\Http\Controllers\VTUcontroller::class,'product_categories'])->name("all_categories");
-Route::get('/all_service',[App\Http\Controllers\VTUcontroller::class,'allServices'])->name("all_service");
-Route::get('/',[App\Http\Controllers\VTUcontroller::class,'index']);
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::post('/',[App\Http\Controllers\TextController::class,'text'])->name('texter');
